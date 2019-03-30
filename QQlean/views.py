9103f8578c4e355fc6dde47cb1b4ab09f0d6e2f3
root <root@anriona.tp.internet.beeline.ru>
2019-03-30 19:44:24 +0300

    Add index

--- QQlean/views.py
+++ QQlean/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index-landingpage.html')
+    return render(request, 'index.html')
