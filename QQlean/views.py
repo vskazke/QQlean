783cabfbbf9477af13e2b83d59d4072509c30730
Hesoyamich <48620599+Hesoyamich@users.noreply.github.com>
2019-03-31 16:21:56 +0300

    add landing

--- QQlean/views.py
+++ QQlean/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def indexlandingpage(request):
+    return render(request, 'index-landingpage.html')
+
