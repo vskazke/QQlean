ae0d210dfe785e10afcb97348451ba09834254ea
root <root@anriona.tp.internet.beeline.ru>
2019-04-08 15:47:20 +0300

    Settings

--- QQlean/settings.py
+++ QQlean/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static/"),
      ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # send mail
 
 EMAIL_HOST = 'smtp.yandex.ru'
