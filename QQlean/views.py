e1ad1be806d397480dfa93a612885a4948ffe38f
root <root@anriona.tp.internet.beeline.ru>
2019-04-10 17:34:39 +0300

    Change forms

--- QQlean/views.py
+++ QQlean/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail, BadHeaderError
@@ -34,10 +34,32 @@ def index(request):
                           settings.EMAIL_HOST_USER, ['aleksauto.info@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
-            return HttpResponse('sucsess')
+            return HttpResponseRedirect('/')
         else:
             print('No')
             return HttpResponse('Make sure all fields are entered and valid.')
+    shortform = ShortForm(request.POST or None)
+    #  form_captcha = DemoForm(request.POST or None)
+    if request.method == "POST" and shortform.is_valid():
+        new = shortform.save()
+        phonesh = request.POST['phone']
+            
+        if phonesh:
+            try:
+                send_mail(phonesh, "phone: %s" % (phonesh),
+                          settings.EMAIL_HOST_USER, ['aleksauto.info@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return HttpResponseRedirect('/')
+        else:
+            print('No')
+            return HttpResponse('Make sure all fields are entered and valid.')
+
+    return render(request, 'index.html', locals())
+
+
+@csrf_exempt
+def result(request):
     shortform = ShortForm(request.POST or None)
     #  form_captcha = DemoForm(request.POST or None)
     if request.method == "POST" and shortform.is_valid():
@@ -57,29 +79,6 @@ def index(request):
         else:
             print('No')
             return HttpResponse('Make sure all fields are entered and valid.')
-
-    return render(request, 'index.html', locals())
-
-
-@csrf_exempt
-def result(request):
-    phone = request.POST['phone']
-    summ_room = request.POST['summ_room']
-    summ_bath = request.POST['summ_bath']
-    
-    q = CallBack(phone=phone, summ_room=summ_room, summ_bath=sum_bath)
-    q.save()
-    prin('llll')
-    if phone:
-        try:
-            send_mail(phone, "phone: %s" % (phone),
-                      settings.EMAIL_HOST_USER, ['aleksauto.info@gmail.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponse('sucsess')
-    else:
-        print('No')
-        return HttpResponse('Make sure all fields are entered and valid.')
 
 
 
